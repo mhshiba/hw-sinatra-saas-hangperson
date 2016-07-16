67ffae63eaa016b50696f70416ad514be97dd8ad
Marcelo Shiba <marcelo.shiba@gmail.com>
2016-07-16 02:55:58 +0000

    Implement guessing a letter

--- app.rb
+++ app.rb
@@ -40,6 +40,8 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    @game.guess letter
+    
     redirect '/show'
   end
   
